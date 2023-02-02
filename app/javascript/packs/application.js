91e74f9365a0ddb79df74ad97646263182d080d5
dezmorales <kotmax98@yandex.ru>
2023-02-02 13:52:15 +0300

    add multiple link to Answer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "./answers"
 import "./questions"
+import '@nathanvda/cocoon'
 
 //= require jquery
 //= require jquery_ujs
