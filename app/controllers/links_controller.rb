d5b71f77079da0313ad43b8908b1d60f76618ddc
dezmorales <kotmax98@yandex.ru>
2023-02-08 11:57:53 +0300

    fix

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,6 @@
 class LinksController < ApplicationController
   before_action :authenticate_user!
+
   def destroy
     @link = Link.find(params[:id])
     if current_user.author_of?(@link.linkable)
