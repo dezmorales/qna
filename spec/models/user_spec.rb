03bed33894bc81b4aecc52cdd3ace51c21ec9502
dezmorales <kotmax98@yandex.ru>
2023-02-22 12:18:52 +0300

    add spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,4 +23,16 @@ RSpec.describe User, type: :model do
     it { expect(user.author_of?(answer)).to be_falsey }
     it { expect(user.author_of?(question)).to be_falsey }
   end
+
+  describe '.find_for_oauth' do
+    let!(:user) { create(:user) }
+    let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456') }
+    let(:service) { double('FindForOauth') }
+
+    it 'calls FindForOauth' do
+      expect(FindForOauth).to receive(:new).with(auth).and_return(service)
+      expect(service).to receive(:call)
+      User.find_for_oauth(auth)
+    end
+  end
 end
